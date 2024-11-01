610fc30d50f7e1eeb3d898b4ef181e6d6a1f5090
Admin <matin.mshah@gmail.com>
2024-11-01 19:14:06 +0330

    fixing apps.py bug

--- Tour/apps.py
+++ Tour/apps.py
@@ -10,6 +10,3 @@ from django.apps import AppConfig
 class TourConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Tour'
-
-    def ready(self):
-        import Tour.signals
