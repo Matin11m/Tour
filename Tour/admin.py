cae82e6cd258b6913cce640dd4ee00c000f87fd5
Admin <matin.mshah@gmail.com>
2024-11-19 14:24:36 +0330

    Remove Province model from project

--- Tour/admin.py
+++ Tour/admin.py
@@ -94,8 +94,8 @@ class UserProfileAdmin(admin.ModelAdmin):
 
 @admin.register(City)
 class CityAdmin(admin.ModelAdmin):
-    list_display = ('name', )
-    search_fields = ('name', )
+    list_display = ('name',)
+    search_fields = ('name',)
     list_filter = ()
 
 
