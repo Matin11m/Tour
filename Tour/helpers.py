ca499ef96303f26d95b4aa89850a385544b8f3b4
mmreza <1tapciran@gmail.com>
2024-11-27 11:47:45 +0330

    Disable sending otp code if DEBUG == True

--- Tour/helpers.py
+++ Tour/helpers.py
@@ -4,16 +4,17 @@ from django.conf import settings
 
 
 def send_otp(phone, verification_code):
-    try:
-        api = KavenegarAPI(settings.KAVEHNEGAR_API_KEY)
-        params = {
-            'sender': settings.KAVEHNEGAR_SENDER,
-            'receptor': phone,
-            'message': f'کد فعال سازی شما {verification_code} است. به هیچ عنوان در اختیار دیگران قرار ندهید!'
-        }   
-        response = api.sms_send(params)
-        return response
-    except APIException as e:
-        return str(e)
-    except HTTPException as e: 
-        return str(e)
+    if not settings.DEBUG:
+        try:
+            api = KavenegarAPI(settings.KAVEHNEGAR_API_KEY)
+            params = {
+                'sender': settings.KAVEHNEGAR_SENDER,
+                'receptor': phone,
+                'message': f'کد فعال سازی شما {verification_code} است. به هیچ عنوان در اختیار دیگران قرار ندهید!'
+            }   
+            response = api.sms_send(params)
+            return response
+        except APIException as e:
+            return str(e)
+        except HTTPException as e: 
+            return str(e)
