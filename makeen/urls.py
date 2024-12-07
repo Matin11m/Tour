08de0a280beaebe8434878c516b066c467032805
Admin <matin.mshah@gmail.com>
2024-12-07 11:38:28 +0330

    fix redoc and swagger

--- makeen/urls.py
+++ makeen/urls.py
@@ -2,9 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+# from rest_framework import permissions
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
 # from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 from rest_framework_simplejwt.views import (
@@ -15,19 +15,19 @@ from rest_framework_simplejwt.views import (
 from Tour import views
 
 from Tour.views import send_sms, verify_code
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Snippets API",
-        default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
+#
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="Snippets API",
+#         default_version='v1',
+#         description="Test description",
+#         terms_of_service="https://www.google.com/policies/terms/",
+#         contact=openapi.Contact(email="contact@snippets.local"),
+#         license=openapi.License(name="BSD License"),
+#     ),
+#     public=True,
+#     permission_classes=(permissions.AllowAny,),
+# )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -42,10 +42,10 @@ urlpatterns = [
     # path('api/send-sms/', send_sms.as_view(), name='send_sms'),
     # path('api/verify-code/', verify_code.as_view(), name='verify_code'),
 
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    # path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # # path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     # path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
