bfcf849baf806da48fee1625e79a67341b245ee0
Admin <matin.mshah@gmail.com>
2024-12-07 11:34:42 +0330

    fix redoc and swagger

--- makeen/urls.py
+++ makeen/urls.py
@@ -1,27 +1,11 @@
-"""
-URL configuration for makeen project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-# from rest_framework import permissions
-# from drf_yasg.views import get_schema_view
-# from drf_yasg import openapi
-# from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -30,20 +14,20 @@ from rest_framework_simplejwt.views import (
 
 from Tour import views
 
-# from Tour.views import send_sms, verify_code
-#
-# schema_view = get_schema_view(
-#     openapi.Info(
-#         title="Snippets API",
-#         default_version='v1',
-#         description="Test description",
-#         terms_of_service="https://www.google.com/policies/terms/",
-#         contact=openapi.Contact(email="contact@snippets.local"),
-#         license=openapi.License(name="BSD License"),
-#     ),
-#     public=True,
-#     permission_classes=(permissions.AllowAny,),
-# )
+from Tour.views import send_sms, verify_code
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -58,9 +42,9 @@ urlpatterns = [
     # path('api/send-sms/', send_sms.as_view(), name='send_sms'),
     # path('api/verify-code/', verify_code.as_view(), name='verify_code'),
 
-    # path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    # path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     # path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     # path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
